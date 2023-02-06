c512f82f7e0c0650aa21d7d558556f0d5e774b66
Roman Likhomanov <romanlee87@gmail.com>
2023-02-06 22:24:31 +0300

    lesson-6

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            ABOUT PAGE
+        </div>
+    );
+};
+
+AboutPage.propTypes = {};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            MAIN PAGE
+        </div>
+    );
+};
+
+MainPage.propTypes = {};
+
+export default MainPage;
