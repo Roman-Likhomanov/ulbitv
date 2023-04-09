ef008030363a5ae81427c5fdfe18e979cd563597
Roman Likhomanov <romanlee87@gmail.com>
2023-04-09 11:27:45 +0300

    lesson-28(fix test)

--- src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
-import { ThemeDecorator } from 'shared/storybook/ThemeDecorator/ThemeDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { NotFoundPage } from './NotFoundPage';
 
 export default {
